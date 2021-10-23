ac041a7532a8e413dad7796bc9c575c4694bb809
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2021-10-22 19:19:38 -0700

    Added create playlist feature

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for, request
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -25,9 +26,38 @@ playlists = db.playlists
 def index():
     return redirect('/playlists')
 
-@app.route('/playlists')
+@app.route('/playlists', methods=['GET'])
 def playlists_index():
     return render_template('playlists_index.html', playlists=playlists.find())
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids,
+    }
+    playlists.insert_one(playlist)
+    return redirect(url_for('playlists_index'))
+
+@app.route('/playlists/new')
+def playlists_new():
+    return render_template('playlists_new.html')
+
+@app.route('/playlists/<playlist_id>')
+def playlists_show(playlist_id):
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_show.html', playlist=playlist)
+
+def video_url_creator(ids):
+    videos = []
+    for id in ids:
+        video = 'https://youtube.com/embed/' + id
+        videos.append(video)
+    return videos
 
 if __name__ == '__main__':
     app.run(debug=True)
